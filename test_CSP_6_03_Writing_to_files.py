a15647ae8c38586ebc380011149576d0ede4eb03
JackA-77 <jalexander28@bentleyschool.org>
2026-05-29 19:12:37 -0700

    Create test_CSP_6_03_Writing_to_files.py

---
+++ test_CSP_6_03_Writing_to_files.py
@@ -0,0 +1,15 @@
+import CSP_6_03_Writing_to_files as HW
+list0 = ['swing', 'your', 'arms', 'from', 'side', 'to', 'side']
+list1 = ['swing your arms', 'from', 'side to side']
+list2 = []
+
+def test_writeFile():
+    assert HW.writeFile(list0, 'doTheMario0.txt') == 'swing\nyour\narms\nfrom\nside\nto\nside\n'
+    assert HW.writeFile(list1, 'doTheMario1.txt') == 'swingyourarms\nfrom\nsidetoside\n'
+    assert HW.writeFile(list2, 'empty.txt') == ''
+
+def test_sortNames():
+    assert HW.sortNames("names.txt","namesNew.txt") == 'Daisy\nLuigi\nMario\nPeach\nToad\nWaluigi\nWario\n'
+
+def test_highScore():
+    assert HW.highScore(14) == 50
