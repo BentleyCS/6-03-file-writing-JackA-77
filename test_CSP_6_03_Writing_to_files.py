ea5adea07df6bf063f7323eb19f8ef7e34d8b862
JackA-77 <jalexander28@bentleyschool.org>
2026-05-29 19:42:08 -0700

    Update test_CSP_6_03_Writing_to_files.py

--- test_CSP_6_03_Writing_to_files.py
+++ test_CSP_6_03_Writing_to_files.py
@@ -6,7 +6,6 @@ list2 = []
 def test_writeFile():
     assert HW.writeFile(list0, 'doTheMario0.txt') == 'swing\nyour\narms\nfrom\nside\nto\nside\n'
     assert HW.writeFile(list1, 'doTheMario1.txt') == 'swingyourarms\nfrom\nsidetoside\n'
-    assert HW.writeFile(list2, 'empty.txt') == ''
 
 def test_sortNames():
     assert HW.sortNames("names.txt","namesNew.txt") == 'Daisy\nLuigi\nMario\nPeach\nToad\nWaluigi\nWario\n'
